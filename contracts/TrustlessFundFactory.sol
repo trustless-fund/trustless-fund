90cea2aefab9ff007124d1f9911b6ae44e97238d
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-18 13:16:06 -0700

    Clean up TrustlessFundFactory.sol

--- contracts/TrustlessFundFactory.sol
+++ contracts/TrustlessFundFactory.sol
@@ -3,21 +3,34 @@ pragma solidity 0.5.8;
 import './TrustlessFund.sol';
 
 contract TrustlessFundFactory {
+  /*** STORAGE VARIABLES ***/
+
   mapping(uint => address) funds;
   uint public nextId;
 
-  // Create TrustlessFund
-  function createFund(uint expiration, address beneficiary) public {
+  /*** PURE/VIEW FUNCTIONS ***/
+
+  /**
+    * @dev Given an id, return the corresponding fund address.
+    * @param _id The id of the fund.
+  */
+  function getFund(uint _id) public view returns(address) {
+    return funds[id];
+  }
+
+  /*** OTHER FUNCTIONS ***/
+
+  /**
+    * @dev Deploy a TrustlessFund contract.
+    * @param _expiration Date time in seconds when timelock expires.
+    * @param _beneficiary Address permitted to withdraw funds after unlock.
+  */
+  function createFund(uint _expiration, address _beneficiary) public {
     require(funds[nextId] == address(0), 'id already in use');
-    TrustlessFund fund = new TrustlessFund(expiration, beneficiary, msg.sender);
+    TrustlessFund fund = new TrustlessFund(_expiration, _beneficiary, msg.sender);
     funds[nextId] = address(fund);
     nextId++;
   }
 
   // TODO: Get funds owned by an address
-
-  // Get a fund by id
-  function getFund(uint id) public returns(address) {
-    return funds[id];
-  }
 }
