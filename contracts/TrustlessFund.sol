110f4d33fc3c7138855ed0670f3ee1bc62b9859f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-18 10:31:35 -0700

    Layout the framework for deposits

--- contracts/TrustlessFund.sol
+++ contracts/TrustlessFund.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.5.8;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"
+
 contract TrustlessFund {
   uint expiration;
 
@@ -7,8 +9,19 @@ contract TrustlessFund {
     expiration = _expiration;
   }
 
-  // function deposit(uint _amount) public payable {
-  //   require(msg.value == _amount, 'incorrect amount');
+  function approve(address _token, uint _amount) public {
+    // token.approve(address(this), _amount)
+  }
 
-  // }
+  function deposit(uint _amount, address _token) public payable {
+    if(_token == 0) {
+      require(msg.value == _amount, 'incorrect amount');
+      // Update user balance
+    }
+    else {
+      ERC20 token = ERC20(_token);
+      token.transferFrom(msg.sender, address(this), _amount);
+      // Update user balance
+    }
+  }
 }
