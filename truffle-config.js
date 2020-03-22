eee903c08c0b617f14d4237d4548c758a294d76e
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-21 20:09:14 -0700

    Modify truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,30 @@
 const path = require("path");
+const keys = require('./keys');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(keys.privKey, `https://ropsten.infura.io/v3/${keys.infura}`);
+      },
+      network_id: 3
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(keys.privKey, `https://rinkeby.infura.io/v3/${keys.infura}`);
+      },
+      network_id: 4
+    },
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider(keys.privKey, `https://mainnet.infura.io/v3/${keys.infura}`);
+      },
+      network_id: 1
     }
   }
 };
