b7a71846076c9ff322bc8b7c6c60321aa500e323
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-26 14:03:24 -0700

    Setup redux devtools

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    )
   );
 }
