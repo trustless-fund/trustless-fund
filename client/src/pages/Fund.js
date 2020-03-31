f22012b46e9e980deb755d643fc7e12e420a7a95
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-03-31 12:01:38 -0700

    Setup drizzle on fund page

--- client/src/pages/Fund.js
+++ client/src/pages/Fund.js
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
+import { DrizzleContext } from "@drizzle/react-plugin";
+import { Drizzle } from "@drizzle/store";
+
+import TrustlessFund from '../contracts/TrustlessFund.json';
+import TrustlessFundFactory from '../contracts/TrustlessFundFactory.json';
+// import ERC20 from '../contracts/ERC20.json';
+
+const drizzleOptions = {
+  contracts: [
+    TrustlessFund,
+    TrustlessFundFactory,
+    // ERC20
+  ], 
+  events: {
+    TrustlessFund: [
+      'Deposit',
+      'Withdraw', 
+      'IncreaseTime', 
+      'UpdateBeneficiary'
+    ],
+    TrustlessFundFactory: [
+      'CreateFund'
+    ],
+    // ERC20: [
+    //   'Transfer',
+    //   'Approval'
+    // ]
+  }
+}
+
+const drizzle = new Drizzle(drizzleOptions);
 
 class Fund extends Component {
   render() {
     return (
-      <div className="fund">
-        <h1>Trust Fund</h1>
-      </div>
+      <DrizzleContext.Provider drizzle={drizzle}>
+        <DrizzleContext.Consumer>
+          {drizzleContext => {
+            const {drizzle, drizzleState, initialized} = drizzleContext;
+
+            if(!initialized) {
+              return "Loading..."
+            }
+
+            return(
+              <div className="fund">
+                <h1>Trust Fund</h1>
+              </div>
+            );
+          }}
+        </DrizzleContext.Consumer>
+      </DrizzleContext.Provider>
     );
   }
 }
