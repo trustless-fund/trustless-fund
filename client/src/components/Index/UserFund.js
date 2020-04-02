64a9f187dacbc2fb4775035cf19073793b7d3fca
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-02 10:19:54 -0700

    Style user funds component

--- client/src/components/Index/UserFund.js
+++ client/src/components/Index/UserFund.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 
+import link from '../../assets/link.svg';
+
 class UserFund extends Component {
   render() {
     return (
-      <a href={`/fund/${this.props.id}`}>
-        <li>
+      <a href={`/fund/${this.props.id}`} className="user-funds__link">
+        <li className="user-funds__fund">
           Fund {this.props.id}
+          <img src={link} alt="Link icon" className="user-funds__icon" />
         </li>
       </a>
     );
--- client/src/components/Index/UserFunds.js
+++ client/src/components/Index/UserFunds.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import UserFund from './UserFund';
 
+import '../../layout/components/userfunds.sass';
+
 class UserFunds extends Component {
   constructor(props) {
     super(props);
