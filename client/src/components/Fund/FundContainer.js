a055f06da4e97d94b1e2c2aea4a3e8aaee799af9
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-02 16:15:12 -0700

    Setup deposit and withdrawal buttons

--- client/src/components/Fund/FundContainer.js
+++ client/src/components/Fund/FundContainer.js
@@ -5,6 +5,7 @@ import WithdrawForm from './WithdrawForm';
 import Assets from './Assets';
 import Expiration from './Expiration';
 import Details from './Details';
+import Button from '../Shared/Button';
 
 import '../../layout/components/fund.sass';
 
@@ -12,10 +13,14 @@ class FundContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      invalidFund: false
+      invalidFund: false,
+      renderWithdrawal: false,
+      depositModal: false,
+      withdrawalModal: false
     }
 
     this.setFundAddress();
+    this.renderWithdrawal();
   }
 
   setFundAddress = async () => {
@@ -32,6 +37,31 @@ class FundContainer extends Component {
     }
   }
 
+  getExpiration = async () => {
+    const expiration = await this.props.drizzle.contracts.TrustlessFund.methods.expiration().call();
+    return expiration;
+  }
+
+  getBeneficiary = async () => {
+    const beneficiary = await this.props.drizzle.contracts.TrustlessFund.methods.beneficiary().call();
+    return beneficiary;
+  }
+
+  renderWithdrawal = async () => {
+    const beneficiary = await this.getBeneficiary();
+    const expiration = await this.getExpiration();
+    const ts = Math.round((new Date()).getTime() / 1000);
+
+    if(beneficiary === this.props.drizzleState.accounts[0] && expiration < ts) {
+      this.setState({renderWithdrawal: true});
+    }
+  }
+
+  renderDepositModal = () => {
+    console.log('renderDepositModal')
+    this.setState({depositModal: true});
+  }
+
   render() {
     if(this.state.invalidFund) {
       return (<InvalidFund />);
@@ -41,8 +71,24 @@ class FundContainer extends Component {
       <div className="fund">
         <Expiration drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
         <Assets drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
-        <DepositForm drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
-        <WithdrawForm drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
+        <div className="fund__buttons">
+          {/* TODO: Add on click and additional class */}
+          <Button 
+            text="Deposit" 
+            class="solid fund__button" 
+            link={null} 
+            button={true} 
+          />
+          {this.state.renderWithdrawal &&
+            <Button text="Withdraw" class="outline fund__button" link={null} button={true} />
+          }
+        </div>
+        {this.state.depositModal &&
+          <DepositForm drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
+        }
+        {this.state.withdrawalModal &&
+          <WithdrawForm drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
+        }
         <Details drizzle={this.props.drizzle} drizzleState={this.props.drizzleState} />
       </div>
     );
--- client/src/components/Fund/WithdrawForm.js
+++ client/src/components/Fund/WithdrawForm.js
@@ -6,10 +6,7 @@ class WithdrawForm extends Component {
     this.state = {
       token: '',
       amount: '',
-      render: false
     }
-
-    this.renderForm();
   }
 
   handleTokenChange = (e) => {
@@ -31,54 +28,31 @@ class WithdrawForm extends Component {
     });
   }
 
-  getExpiration = async () => {
-    const expiration = await this.props.drizzle.contracts.TrustlessFund.methods.expiration().call();
-    return expiration;
-  }
-
-  getBeneficiary = async () => {
-    const beneficiary = await this.props.drizzle.contracts.TrustlessFund.methods.beneficiary().call();
-    return beneficiary;
-  }
-
-  renderForm = async () => {
-    const beneficiary = await this.getBeneficiary();
-    const expiration = await this.getExpiration();
-    const ts = Math.round((new Date()).getTime() / 1000);
-
-    if(beneficiary === this.props.drizzleState.accounts[0] && expiration < ts) {
-      this.setState({render: true});
-    }
-  }
-
   render() {
-    if(this.state.render) {
-      return (
-        <div>
-          <h2>Withdraw Form</h2>
-          <form onSubmit={this.handleSubmit}>
-            <input 
-              type="text"
-              className="form__input"
-              placeholder="Token"
-              onChange={this.handleTokenChange}
-              value={this.state.token}
-            />
-            <input 
-              type="number"
-              className="form__input"
-              placeholder="Amount"
-              onChange={this.handleAmountChange}
-              value={this.state.amount}
-            />
-            <button className="form__button">
-              Submit
-            </button>
-          </form>
-        </div>
-      );
-    }
-    return null;
+    return (
+      <div>
+        <h2>Withdraw Form</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text"
+            className="form__input"
+            placeholder="Token"
+            onChange={this.handleTokenChange}
+            value={this.state.token}
+          />
+          <input 
+            type="number"
+            className="form__input"
+            placeholder="Amount"
+            onChange={this.handleAmountChange}
+            value={this.state.amount}
+          />
+          <button className="form__button">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
