244f2d503a78a2a93ae70825f9438f86bf01496f
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-02 09:12:21 -0700

    Style nav

--- client/src/components/Shared/Nav.js
+++ client/src/components/Shared/Nav.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 
+import logo from '../../assets/logo.png';
+
+import '../../layout/components/nav.sass';
+
 class Nav extends Component {
   constructor(props) {
     super(props);
@@ -10,9 +14,12 @@ class Nav extends Component {
 
   render() {
     return (
-      <nav>
-        <h1>Trustless Fund</h1>
-        <button>
+      <nav className="nav">
+        <h1 className="nav__header">
+          <img src={logo} alt="Trustless Fund" className="nav__logo" />
+          Trustless Fund
+        </h1>
+        <button className="nav__button">
           {this.state.address ? 
             `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
             'Connect Wallet'}
