25869cd164979032aa648099613ce930b5d21861
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-02 19:58:42 -0700

    Get human readable date

--- client/src/components/Fund/Expiration.js
+++ client/src/components/Fund/Expiration.js
@@ -10,9 +10,30 @@ class Expiration extends Component {
     this.getExpiration();
   }
 
+  months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
   getExpiration = async () => {
     const expiration = await this.props.drizzle.contracts.TrustlessFund.methods.expiration().call();
-    this.setState({expiration});
+    const dateObject = new Date(expiration * 1000);
+    const monthNum = dateObject.getUTCMonth();
+    const month = this.months[monthNum];
+    const date = dateObject.getUTCDate();
+    const year = dateObject.getUTCFullYear();
+    const fullDate = `${month} ${date}, ${year}`;
+    this.setState({expiration: fullDate});
   }
 
   render() {
