8e4db2dd40c0613dd89e1d2a945770e4c8f58683
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-06 10:12:07 -0700

    Get usd value of tokens

--- client/src/components/Fund/Asset.js
+++ client/src/components/Fund/Asset.js
@@ -6,18 +6,50 @@ class Asset extends Component {
     super(props);
     this.state = {
       token: null,
-      balance: null
+      balance: null,
+      usdValue: 0
     }
   }
 
   componentDidMount = () => {
     this.getToken();
     this.getBalance();
+    this.getUsdValue();
   }
 
-  // getLogo = () => {
-  //   const logo = 
-  // }
+  // Source: https://stackoverflow.com/a/32108184/13171993
+  isEmpty = (obj) => {
+    for(var prop in obj) {
+      if(obj.hasOwnProperty(prop)) {
+        return false;
+      }
+    }
+  
+    return JSON.stringify(obj) === JSON.stringify({});
+  }
+
+  getUsdValue = async () => {
+    if(this.props.token.address === '0x0000000000000000000000000000000000000000') {
+      fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+        .then((res) => {
+          return res.json();
+        }).then((res) => {
+          const usdValue = res.ethereum.usd;
+          this.setState({usdValue});
+        });
+    } else {
+      fetch(`https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${this.props.token.address}&vs_currencies=usd`)
+        .then((res) => {
+          return res.json();
+        }).then((res) => {
+          if(this.isEmpty(res)) {
+            return;
+          }
+          const usdValue = res[this.props.token.address].usd;
+          this.setState({usdValue});
+        });
+    }
+  }
 
   getToken = async () => {
     if(this.props.token.address === '0x0000000000000000000000000000000000000000') {
@@ -46,7 +78,7 @@ class Asset extends Component {
         </p>
         <p className="assets__asset-info assets__asset-info--amount">
           {/* TODO: If balance < 0.0001, return <0.0001 */}
-          {this.state.balance}
+          {this.state.balance}/{`$${this.state.usdValue}`}
           {/* TODO: Get USD value from etherscan api */}
         </p>
       </li>
