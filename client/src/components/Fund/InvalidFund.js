5d5377b120a9f571ee7dd523cded5b721749f7b3
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-06 13:55:04 -0700

    Style invalid fund page

--- client/src/components/Fund/InvalidFund.js
+++ client/src/components/Fund/InvalidFund.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
+import '../../layout/components/invalidfund.sass';
+
 class InvalidFund extends Component {
   render() {
     return (
-      <div>
-        <h2>
+      <div className="invalid-fund">
+        <h2 className="invalid-fund__header">
           Error
         </h2>
-        <p>
-          This fund doesn't seem to exist. Are you sure you have the correct URL?
+        <p className="invalid-fund__info">
+          This fund doesn't seem to exist.
+          <a href="/" className="invalid-fund__link">
+            Go back to homepage.
+          </a>
         </p>
       </div>
     );
