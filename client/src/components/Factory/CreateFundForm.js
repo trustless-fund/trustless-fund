c4af1f10ff94f923b64816c018ce8d5c0b70731d
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-09 16:56:18 -0700

    Disable button if invalid beneficiary address

--- client/src/components/Factory/CreateFundForm.js
+++ client/src/components/Factory/CreateFundForm.js
@@ -10,8 +10,24 @@ class CreateFundForm extends Component {
     invalidAddress: this.props.invalidAddress
   }
 
-  componentWillReceiveProps = (nextProps) => {
-    this.setState({invalidAddress: nextProps.invalidAddress});
+  componentWillReceiveProps = async (nextProps) => {
+    await this.setState({invalidAddress: nextProps.invalidAddress});
+    this.disableButton();
+  }
+
+  disableButton = () => {
+    const fundButton = document.querySelector('.create-fund__button');
+    if(this.state.invalidAddress) {
+      if(!fundButton.classList.contains('disabled')) {
+        fundButton.disabled = true;
+        fundButton.classList.add('disabled');
+      }
+    } else {
+      if(fundButton.classList.contains('disabled')) {
+        fundButton.disabled = false;
+        fundButton.classList.remove('disabled');
+      }
+    }
   }
 
   render() {
