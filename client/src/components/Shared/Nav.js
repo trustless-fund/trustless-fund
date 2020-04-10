a59570ffa9c3ae9eb0f1c172f4c4ea806ffd3617
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-10 10:20:49 -0700

    Better display no web3 error in nav

--- client/src/components/Shared/Nav.js
+++ client/src/components/Shared/Nav.js
@@ -30,26 +30,28 @@ class Nav extends Component {
     if(this.state.noProvider) {
       return (
         <>
-          <p className="nav__error">
-            No web3 detected. Please download and connect to 
-            <a 
-              href="https://metamask.io/"
-              className="nav__error-link"
-              target="_blank"
-              rel="noopener noreferrer">
-              Metamask.
-            </a>
-          </p>
-          <nav className="nav">
-            <a href="/" className="nav__header">
-              <img src={logo} alt="Trustless Fund" className="nav__logo" />
-              Trustless Fund
-            </a>
-            <button className="nav__button">
-              {this.state.address ? 
-                `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
-                'Connect Wallet'}
-            </button>
+          <nav className="nav-error">
+            <p className="nav-error__error">
+              No web3 detected. Please download and connect to 
+              <a 
+                href="https://metamask.io/"
+                className="nav-error__link"
+                target="_blank"
+                rel="noopener noreferrer">
+                Metamask.
+              </a>
+            </p>
+            <div className="nav-error__content">
+              <a href="/" className="nav__header">
+                <img src={logo} alt="Trustless Fund" className="nav__logo" />
+                Trustless Fund
+              </a>
+              <button className="nav__button">
+                {this.state.address ? 
+                  `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
+                  'Connect Wallet'}
+              </button>
+            </div>
           </nav>
         </>
       );
