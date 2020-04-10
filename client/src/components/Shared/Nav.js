13ba6cc84d5cb17c448df00bccea7c27beb92ea5
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-10 09:45:04 -0700

    Add nav error for no web3

--- client/src/components/Shared/Nav.js
+++ client/src/components/Shared/Nav.js
@@ -8,7 +8,8 @@ class Nav extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      address: this.props.drizzleState.accounts[0]
+      address: this.props.drizzleState.accounts[0],
+      noProvider: false
     }
 
     if(window.ethereum) {
@@ -21,10 +22,39 @@ class Nav extends Component {
   componentDidMount = () => {
     if(!this.props.drizzle.web3.givenProvider) {
       this.setState({address: null});
+      this.setState({noProvider: true});
     }
   }
 
   render() {
+    if(this.state.noProvider) {
+      return (
+        <>
+          <p className="nav__error">
+            No web3 detected. Please download and connect to 
+            <a 
+              href="https://metamask.io/"
+              className="nav__error-link"
+              target="_blank"
+              rel="noopener noreferrer">
+              Metamask.
+            </a>
+          </p>
+          <nav className="nav">
+            <a href="/" className="nav__header">
+              <img src={logo} alt="Trustless Fund" className="nav__logo" />
+              Trustless Fund
+            </a>
+            <button className="nav__button">
+              {this.state.address ? 
+                `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
+                'Connect Wallet'}
+            </button>
+          </nav>
+        </>
+      );
+    }
+
     return (
       <nav className="nav">
         <a href="/" className="nav__header">
