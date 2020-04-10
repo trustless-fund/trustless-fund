5fdd56cb1e726d676fc14dcbb0721f235342ac52
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-10 10:47:03 -0700

    Add testnet error in nav

--- client/src/components/Shared/Nav.js
+++ client/src/components/Shared/Nav.js
@@ -9,7 +9,8 @@ class Nav extends Component {
     super(props);
     this.state = {
       address: this.props.drizzleState.accounts[0],
-      noProvider: false
+      noProvider: false,
+      testNetwork: false
     }
 
     if(window.ethereum) {
@@ -23,37 +24,76 @@ class Nav extends Component {
     if(!this.props.drizzle.web3.givenProvider) {
       this.setState({address: null});
       this.setState({noProvider: true});
+    } else {
+      this.setState({address: this.props.drizzleState.accounts[0]});
+      this.setState({noProvider: false});
+    }
+    if(this.props.drizzleState.web3.networkId !== 1) {
+      this.setState({testNetwork: true});
+    } else {
+      this.setState({testNetwork: false})
     }
   }
 
+  getNetwork = (id) => {
+    const networks = {
+      1: 'Mainnet',
+      3: 'Ropsten',
+      4: 'Rinkeby',
+      5: 'Goerli',
+      42: 'Kovan'
+    }
+
+    return networks[id];
+  }
+
   render() {
+    if(this.state.testNetwork) {
+      return (
+        <nav className="nav-error">
+          <p className="nav-error__error">
+            Note: You are currently connected to the {this.getNetwork(this.props.drizzleState.web3.networkId)} testnet.
+          </p>
+          <div className="nav-error__content">
+            <a href="/" className="nav__header">
+              <img src={logo} alt="Trustless Fund" className="nav__logo" />
+              Trustless Fund
+            </a>
+            <button className="nav__button">
+              {this.state.address ? 
+                `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
+                'Connect Wallet'}
+            </button>
+          </div>
+        </nav>
+      );
+    }
+
     if(this.state.noProvider) {
       return (
-        <>
-          <nav className="nav-error">
-            <p className="nav-error__error">
-              No web3 detected. Please download and connect to 
-              <a 
-                href="https://metamask.io/"
-                className="nav-error__link"
-                target="_blank"
-                rel="noopener noreferrer">
-                Metamask.
-              </a>
-            </p>
-            <div className="nav-error__content">
-              <a href="/" className="nav__header">
-                <img src={logo} alt="Trustless Fund" className="nav__logo" />
-                Trustless Fund
-              </a>
-              <button className="nav__button">
-                {this.state.address ? 
-                  `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
-                  'Connect Wallet'}
-              </button>
-            </div>
-          </nav>
-        </>
+        <nav className="nav-error">
+          <p className="nav-error__error">
+            No web3 detected. Please download and connect to 
+            <a 
+              href="https://metamask.io/"
+              className="nav-error__link"
+              target="_blank"
+              rel="noopener noreferrer">
+              Metamask.
+            </a>
+          </p>
+          <div className="nav-error__content">
+            <a href="/" className="nav__header">
+              <img src={logo} alt="Trustless Fund" className="nav__logo" />
+              Trustless Fund
+            </a>
+            <button className="nav__button">
+              {this.state.address ? 
+                `${this.state.address.slice(0, 4)}...${this.state.address.slice(this.state.address.length - 4, this.state.address.length)}` : 
+                'Connect Wallet'}
+            </button>
+          </div>
+        </nav>
       );
     }
 
