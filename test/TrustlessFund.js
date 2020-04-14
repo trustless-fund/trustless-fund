f4846259250e39ba99b0f02903f6798adec04278
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-14 16:08:20 -0700

    Withdrawal tests

--- test/TrustlessFund.js
+++ test/TrustlessFund.js
@@ -17,8 +17,8 @@ describe('TrustlessFund', () => {
   describe('Deposits', () => {
     it('returns correct amount of deposited ETH', async () => {
       await this.fund.deposit('100', constants.ZERO_ADDRESS, {from: owner, value: '100'});
-      const tokens = await this.fund.tokens(constants.ZERO_ADDRESS);
-      const balance = await tokens.balance.toString();
+      const token = await this.fund.tokens(constants.ZERO_ADDRESS);
+      const balance = await token.balance.toString();
       expect(balance).to.equal('100');
     });
 
@@ -39,5 +39,48 @@ describe('TrustlessFund', () => {
     });
 
     // Add additional test for ERC20s
+  });
+
+  describe('Withdrawals', () => {
+    beforeEach(async () => {
+      await this.fund.deposit('100', constants.ZERO_ADDRESS, {from: owner, value: '100'});
+    });
+
+    it('successfully withdraws eth', async () => {
+      await this.fund.withdraw('50', constants.ZERO_ADDRESS, {from: beneficiary});
+      const token = await this.fund.tokens(constants.ZERO_ADDRESS);
+      const balance = await token.balance.toString();
+      expect(balance).to.equal('50');
+    });
+
+    it('reverts if msg.sender is not beneficiary', async () => {
+      await expectRevert(
+        this.fund.withdraw('50', constants.ZERO_ADDRESS, {from: user}),
+        'only the beneficiary can perform this function'
+      );
+    });
+
+    it('reverts if not enough balance', async () => {
+      await expectRevert(
+        this.fund.withdraw('101', constants.ZERO_ADDRESS, {from: beneficiary}),
+        'not enough balance'
+      );
+    });
+
+    describe('Locked', () => {
+      let lockedFund;
+      beforeEach(async () => {
+        // Much greater than current time
+        lockedFund = await TrustlessFund.new(15869055290, beneficiary, owner, {from: owner});
+        await lockedFund.deposit('100', constants.ZERO_ADDRESS, {from: owner, value: '100'});
+      });
+
+      it('reverts if withdraw period not expired', async () => {
+        await expectRevert(
+          lockedFund.withdraw('100', constants.ZERO_ADDRESS, {from: beneficiary}),
+          'contract is still locked'
+        );
+      });
+    });
   });
 });
