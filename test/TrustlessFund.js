557615a3a9d15ae1a59b7dde8c6754307bf4c4f6
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-14 15:42:48 -0700

    Deposit tests

--- test/TrustlessFund.js
+++ test/TrustlessFund.js
@@ -1,6 +1,12 @@
 const { accounts, contract } = require('@openzeppelin/test-environment');
 const [ owner, beneficiary, user ] = accounts;
 const { expect } = require('chai');
+const {
+  constants,
+  expectEvent,
+  expectRevert
+} = require('@openzeppelin/test-helpers');
+
 const TrustlessFund = contract.fromArtifact('TrustlessFund');
 
 describe('TrustlessFund', () => {
@@ -9,11 +15,29 @@ describe('TrustlessFund', () => {
   });
 
   describe('Deposits', () => {
-    it('displays correct amount of deposited ETH', async () => {
-      await this.fund.deposit('100', '0x0000000000000000000000000000000000000000', {from: owner, value: '100'});
-      const tokens = await this.fund.tokens('0x0000000000000000000000000000000000000000');
+    it('returns correct amount of deposited ETH', async () => {
+      await this.fund.deposit('100', constants.ZERO_ADDRESS, {from: owner, value: '100'});
+      const tokens = await this.fund.tokens(constants.ZERO_ADDRESS);
       const balance = await tokens.balance.toString();
       expect(balance).to.equal('100');
     });
+
+    it('reverts if not supplied with correct amount of ETH', async () => {
+      await expectRevert(
+        this.fund.deposit('100', constants.ZERO_ADDRESS, {from: owner, value: '50'}),
+        'incorrect amount'
+      );
+    });
+
+    it('emits the deposit event', async () => {
+      const receipt = await this.fund.deposit('100', constants.ZERO_ADDRESS, {from: owner, value: '100'});
+      expectEvent(receipt, 'Deposit', {
+        _from: owner,
+        _value: '100',
+        _token: constants.ZERO_ADDRESS
+      });
+    });
+
+    // Add additional test for ERC20s
   });
 });
