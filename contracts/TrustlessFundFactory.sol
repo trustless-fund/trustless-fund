def493adbf47abd8ba985bed1b78e110ad64c796
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-04-16 11:37:15 -0700

    Update factory contract to remove fees

--- contracts/TrustlessFundFactory.sol
+++ contracts/TrustlessFundFactory.sol
@@ -21,17 +21,6 @@ contract TrustlessFundFactory is Ownable {
   */
   uint public nextId;
 
-  /**
-    * @notice The fee, in wei, charged to open a contract.
-              Note: This will remain 0 indefinitely.
-  */
-  uint public fee;
-
-  /**
-    * @notice The total amount of fees, in wei, collected.
-  */
-  uint public feesAccrued;
-
   /*** EVENTS ***/
 
   /**
@@ -61,13 +50,6 @@ contract TrustlessFundFactory is Ownable {
     return userFunds[_user];
   }
 
-  /**
-    * @dev Returns the fee, in wei, to open a contract.
-  */
-  function getFee() public view returns(uint) {
-    return fee;
-  }
-
   /*** OTHER FUNCTIONS ***/
 
   /**
@@ -75,34 +57,12 @@ contract TrustlessFundFactory is Ownable {
     * @param _expiration Date time in seconds when timelock expires.
     * @param _beneficiary Address permitted to withdraw funds after unlock.
   */
-  function createFund(uint _expiration, address _beneficiary) public payable {
+  function createFund(uint _expiration, address _beneficiary) public {
     require(funds[nextId] == address(0), 'id already in use');
-    require(msg.value == fee, 'must pay fee');
     TrustlessFund fund = new TrustlessFund(_expiration, _beneficiary, msg.sender);
-    feesAccrued += fee;
     funds[nextId] = address(fund);
     userFunds[msg.sender].push(nextId);
     nextId++;
     emit CreateFund(_expiration, _beneficiary, msg.sender);
-  }
-
-  /**
-    * @dev Sets the fee, in wei, to open a contract.
-    * @param _newFee The new fee, in wei.
-  */
-  function setFee(uint _newFee) public onlyOwner() {
-    fee = _newFee;
-  }
-
-  /**
-    * @dev Collect accrued fees.
-    * @param _amount The amount to collect, in wei.
-  */
-  function collectFees(uint _amount) public onlyOwner() {
-    require(feesAccrued > 0, 'no fees accrued');
-    require(_amount < feesAccrued, 'not enough fees accrued');
-    feesAccrued -= _amount;
-    (bool success, ) = msg.sender.call.value(_amount)("");
-    require(success, "Transfer failed.");
   }
 }
