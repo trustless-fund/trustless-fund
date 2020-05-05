1170583ea8a97551a3e4adcfcd8a99a714371b5a
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-05-05 16:27:39 -0700

    Update 404 page

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,54 +1,137 @@
 import React, {Component} from 'react';
+import Web3 from 'web3';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import keys from '../keys';
+
 import Nav from '../components/Shared/Nav';
 import Footer from '../components/Shared/Footer';
-import { DrizzleContext } from "@drizzle/react-plugin";
-import { Drizzle } from "@drizzle/store";
-import logo from '../assets/logo.png';
 
 import '../layout/components/loading.sass';
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: keys.infura
+    }
+  }
+}
+
+function initWeb3(provider) {
+  const web3 = new Web3(provider);
+
+  web3.eth.extend({
+    methods: [
+      {
+        name: 'chainId',
+        call: 'eth_chainId',
+        outputFormatter: web3.utils.hexToNumber
+      }
+    ]
+  });
+
+  return web3;
+}
 
 class FourOFour extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      web3: null,
+      provider: null,
+      connected: null,
+      address: null,
+      chainId: null,
+      networkId: null
+    }
 
-    this.drizzle = new Drizzle({});
+    this.web3modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+      providerOptions
+    });
+
+    if (this.web3modal.cachedProvider) {
+      this.onConnect()
+    }
+  } 
+
+  onConnect = async () => {
+    try {
+      const provider = await this.web3modal.connect();
+      await this.subscribeProvider(provider);
+      const web3 = initWeb3(provider);
+      const accounts = await web3.eth.getAccounts();
+      const address = accounts[0];
+      const networkId = await web3.eth.net.getId();
+      const chainId = await web3.eth.chainId();
+
+      await this.setState({
+        web3,
+        provider,
+        connected: true,
+        address,
+        chainId,
+        networkId
+      });
+    } catch {
+      console.log('No web3 provider');
+    }
+  }
+
+  subscribeProvider = async (provider) => {
+    provider.on('close', () => this.disconnect());
+
+    provider.on('accountsChanged', async (accounts) => {
+      await this.setState({ address: accounts[0] });
+    });
+
+    provider.on('chainChanged', async (chainId) => {
+      const { web3 } = this.state
+      const networkId = await web3.eth.net.getId()
+      await this.setState({ chainId, networkId });
+    });
+
+    provider.on('networkChanged', async (networkId) => {
+      const { web3 } = this.state;
+      const chainId = await web3.eth.chainId();
+      await this.setState({ chainId, networkId });
+    });
+  }
+
+  disconnect = async () => {
+    const { web3 } = this.state
+    if (web3 && web3.currentProvider && web3.currentProvider.close) {
+      await web3.currentProvider.close()
+    }
+    await this.web3modal.clearCachedProvider();
+    this.setState({connected: false, address: null});
   }
 
   render() {
     return (
-      <DrizzleContext.Provider drizzle={this.drizzle}>
-        <DrizzleContext.Consumer>
-          {drizzleContext => {
-            const {drizzle, drizzleState, initialized} = drizzleContext;
-
-            if(window.ethereum && !initialized) {
-              return (
-                <div className="loading">
-                  <img src={logo} alt="Trustless Fund" className="loading__image" />
-                </div>
-              );
-            }
-
-            return(
-              <>
-                <Nav drizzle={drizzle} drizzleState={drizzleState} />
-                <div className="invalid-fund">
-                  <h2 className="invalid-fund__header">
-                    Error
-                  </h2>
-                  <p className="invalid-fund__info">
-                    This page doesn't seem to exist.
-                    <a href="/" className="invalid-fund__link">
-                      Go back to homepage.
-                    </a>
-                  </p>
-                </div>
-                <Footer drizzle={drizzle} drizzleState={drizzleState} />
-              </>
-            );
-          }}
-        </DrizzleContext.Consumer>
-      </DrizzleContext.Provider>
+      <>
+        <Nav
+          web3={this.state.web3}
+          address={this.state.address}
+          networkId={this.state.networkId}
+          onConnect={this.onConnect}
+          disconnect={this.disconnect}
+          connected={this.state.connected} />
+          <div className="invalid-fund">
+            <h2 className="invalid-fund__header">
+              Error
+            </h2>
+            <p className="invalid-fund__info">
+              This page doesn't seem to exist.
+              <a href="/" className="invalid-fund__link">
+                Go back to homepage.
+              </a>
+            </p>
+          </div>
+        <Footer />
+      </>
     );
   }
 } 
