374d1382b913f2d5342ff3368ed4f85c7fc44da8
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-05-15 16:36:42 -0700

    Modify provider options on all pages

--- client/src/pages/Factory.js
+++ client/src/pages/Factory.js
@@ -76,7 +76,7 @@ class Factory extends Component {
         }
       }
       return providerOptions;
-    } else {
+    } else if(process.env.NODE_ENV === 'development') {
       await import('../keys').then(async (res) => {
         keys.infura = await res.infura;
         keys.fortmatic = await res.fortmatic;
